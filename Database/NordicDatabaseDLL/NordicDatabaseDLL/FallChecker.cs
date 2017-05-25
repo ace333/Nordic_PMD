f66d9a08d49a8a3ea9462db28b6624102b360bb2
ace333 <achudy333@gmail.com>
2017-05-25 23:16:04 +0200

    miljon zmian, glownie biblioteka i takie tam

---
+++ Database/NordicDatabaseDLL/NordicDatabaseDLL/FallChecker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NordicDatabaseDLL
+{
+    public class FallChecker
+    {
+        private const int border = 5;
+        private bool isFucked;
+
+
+        public bool CheckIfFall(float[] dataX, float[] dataY, float[] dataZ, float delta, int samplesAmount)
+        {
+            float[] validX, validY, validZ;
+
+            if(!CheckIfTabIsLongEnough(dataX) || !CheckIfTabIsLongEnough(dataX) || !CheckIfTabIsLongEnough(dataZ))
+            {
+                return false;
+            }
+
+            //operations on last 100 samples
+            validX = ObtainLastValidValuesFromTab(dataX);
+            validY = ObtainLastValidValuesFromTab(dataY);
+            validZ = ObtainLastValidValuesFromTab(dataZ);
+
+            //next calculations done only when isFucked is false
+            CheckSamples(dataX, delta, samplesAmount);
+            if (!isFucked)
+                CheckSamples(dataY, delta, samplesAmount);
+            if (!isFucked)
+                CheckSamples(dataZ, delta, samplesAmount);
+
+            return isFucked;
+        }
+
+        private void CheckSamples(float[] data, float delta, int samplesAmount)
+        {
+            int maxIndex = border - samplesAmount;
+            float[] calcTab = new float[samplesAmount];
+
+            for(int i=0; i <= maxIndex; i++)
+            {
+                //filling calcTab with data from data Table
+                for(int j=i; j<samplesAmount; j++)
+                {
+                    calcTab[j] = data[j];
+                }
+
+                DoDeltaCalculations(calcTab, delta);
+            }
+
+        }
+
+        private void DoDeltaCalculations(float[] dataToAnalyze, float delta)
+        {
+            for(int i=0; i<dataToAnalyze.Length; i++)
+            {
+                float sample = dataToAnalyze[i];
+
+                for(int j=0; j<dataToAnalyze.Length; j++)
+                {
+                    if(i!=j)
+                    {
+                        float absDelta = Math.Abs(sample) - Math.Abs(dataToAnalyze[j]);
+
+                        if (absDelta > delta)
+                        {
+                            isFucked = true;
+                            return;
+                        }
+                            
+                    }
+                }
+            }
+        }
+
+        private bool CheckIfTabIsLongEnough(float[] tab)
+        {
+            if (tab.Length < border)
+                return false;
+            else
+                return true;
+        }
+
+        private float[] ObtainLastValidValuesFromTab(float[] tab)
+        {
+            float[] validTab = new float[border];
+            int index = tab.Length - border;
+            int iter = 0;
+
+            for(int i=index; i<tab.Length; i++)
+            {
+                validTab[iter++] = tab[i];
+            }
+
+            return validTab;
+        }       
+
+    }
+}
---
+++ Database/NordicDatabaseDLL/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NordicDatabaseDLL;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Detect_Fall()
+        {
+            //arrange
+            float[] X = { 0.24F, 0.13F, 0.13F, 0.13F, 0.13F };
+            float[] Y = { 0.13F, 0.13F, 0.13F, 0.43F, 0.91F };
+            float[] Z = { 2.45F, 0.13F, 0.13F, 0.13F, 0.43F };
+            int amount = 5;
+            float delta = 1;
+            FallChecker check = new FallChecker();
+
+            //act
+            bool result = check.CheckIfFall(X, Y, Z, delta, amount);
+
+            //assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void Can_Select_All_Patients()
+        {
+            //arrange
+            ConnectionClass conn = new ConnectionClass();
+
+            //act
+            conn.OpenConnection();
+            DataTable resutl = conn.SelectAllPatients();
+            conn.CloseConnection();
+
+            //assert    
+            Assert.AreEqual(1, resutl.Rows[0][0]);
+            Assert.AreEqual(2, resutl.Rows[1][0]);
+        }
+
+        [TestMethod]
+        public void Can_Select_Last_X_ID()
+        {
+            //arrange
+            ConnectionClass conn = new ConnectionClass();
+
+            //act
+            conn.OpenConnection();
+            int result = conn.GetLastXAxisID();
+
+            //assert
+            Assert.AreEqual(2040, result);
+        }
+    }
+}
